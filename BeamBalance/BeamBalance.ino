eded3928f2e7b63e30a39fe6296619414541c76c
Kevin Zhang <kevin.zhang.13499@gmail.com>
2015-05-21 08:01:17 -0700

    Add ultrasonic code

--- BeamBalance/BeamBalance.ino
+++ BeamBalance/BeamBalance.ino
@@ -2,15 +2,12 @@
 //global variables
 int leftServoPin = 1;  //port number for left motor
 int rightServoPin = 2; //port numbersfor right motor
-int pulse = 1500; //delay in microseconds
+int right_echo_pin = 12;
+int right_trigger_pin = 13;
 
-int right_motor_speed_pin = 3;
-int right_motor_forward_pin = 4;
-int right_motor_backward_pin = 5;
- 
-int left_motor_speed_pin = 8;
-int left_motor_forward_pin = 9;
-int left_motor_backward_pin = 10;
+int left_echo_pin = 8;
+int left_trigger_pin = 7;
+int pulse = 1500; //delay in microseconds
 
 double distanceFromLeft=0;  //in inches
 double distanceFromRight=0; //in inches
@@ -25,78 +22,67 @@ double lasterror=0;
 double cumError=0;
 //i2c slave address AD0 pin 9 at b1101000 and b1101001
 void setup(){
-  Wire.begin();
-  pinMode(leftServoPin, OUTPUT);
-  pinMode(rightServoPin, OUTPUT);
-  
-  pinMode(right_motor_speed_pin, OUTPUT);
-  pinMode(right_motor_forward_pin, OUTPUT);
-  pinMode(right_motor_backward_pin, OUTPUT);
-  pinMode(left_motor_speed_pin, OUTPUT);
-  pinMode(left_motor_forward_pin, OUTPUT);
-  pinMode(left_motor_backward_pin, OUTPUT);
-  
-  //set all motors to off by default
-  off(left_motor_speed_pin);
-  off(left_motor_forward_pin);
-  off(left_motor_backward_pin);
-  off(right_motor_speed_pin);
-  off(right_motor_forward_pin);
-  off(right_motor_backward_pin);
   Serial.begin(9600);
-}
+  pinMode(right_echo_pin, INPUT);
+  pinMode(right_trigger_pin, OUTPUT);
+  pinMode(left_echo_pin, INPUT);
+  pinMode(left_trigger_pin, OUTPUT);}
+  
 void loop() {
   //Distance will be from left ultrasonic sensor
-  error = getError();
+  double error = getError();
   double slope=error-lasterror;
   cumError=cumError+error;
   double torq = pid(error, cumError, slope, kP, kI, kD);
-
-  go(torq, torq, 50);
 }
 
-double error(){
+double getError(){
   distanceFromLeft=getLeftDistance();
   distanceFromRight=getRightDistance();
   distanceFromLeft=(distanceFromLeft+(TOTAL_LENGTH-distanceFromRight))/2;
-  if(distanceFromleft>TOTAL_LENGTH) distanceFromLeft=0;
+  if(distanceFromLeft>TOTAL_LENGTH) distanceFromLeft=0;
   return target-distanceFromLeft;
 }
 
+double getLeftDistance(){
+  double left_ping_microseconds = ping(left_trigger_pin, left_echo_pin);
+  double left_distance = ping_to_cm(left_ping_microseconds);
+  return left_distance;
+}
+
+double ping_to_cm(int ping_microseconds){
+  double sound_cm_per_microsecond_at_sea_level = 0.034029;
+  return ping_microseconds * sound_cm_per_microsecond_at_sea_level / 2;}
+
+double getRightDistance(){
+  double right_ping_microseconds = ping(right_trigger_pin, right_echo_pin);
+  double right_distance = ping_to_cm(right_ping_microseconds);
+}
 double pid(double error, double integral, double derivative, double Kp, double Ki, double Kd) {
   return (error * Kp + integral * Ki + derivative * Kd);
 }
 
 void on(int pin){
-  digitalWrite(pin, HIGH);
-}
- 
+  digitalWrite(pin, HIGH);}
+
 void off(int pin){
-  digitalWrite(pin, LOW);
-}
+  digitalWrite(pin, LOW);}
 
-void go(int left_motor_speed, int right_motor_speed, int time){
-  set_motor(left_motor_speed_pin, left_motor_forward_pin, left_motor_backward_pin, left_motor_speed);
-  set_motor(right_motor_speed_pin, right_motor_forward_pin, right_motor_backward_pin, right_motor_speed);
-  delay(time);
-  set_motor(left_motor_speed_pin, left_motor_forward_pin, left_motor_backward_pin, 0);
-  set_motor(right_motor_speed_pin, right_motor_forward_pin, right_motor_backward_pin, 0);
-}
- 
+int ping(int trigger, int echo){
+  int ping_time = 0;
+  // turn off trigger
+  off(trigger);
+  delayMicroseconds(2);
+  // turn on the trigger and leave it on long enough for the
+  // sonar sensor to notice
+  on(trigger);
+  delayMicroseconds(10);
+  off(trigger);
+  ping_time = pulseIn(echo, HIGH);
+  if(ping_time <= 0){
+    ping_time = 3000;}
+  // sonar needs some time to recover before pinging again,
+  // so make sure it gets enough sleep right here.  50 milliseconds
+  delay(50);
+  return ping_time;}
 
-void set_motor(int speed_pin, int forward_pin, int backward_pin, int speed){
-  if(speed > 0){
-    off(backward_pin);
-    on(forward_pin);
-  }else if(speed < 0){
-    off(forward_pin);
-    on(backward_pin);
-    speed = -speed;
-  }else{ // speed is 0
-    off(forward_pin);
-    off(backward_pin);
-  }if(speed > 255){
-    speed = 255;
-  }
-  analogWrite(speed_pin, speed);
-}
