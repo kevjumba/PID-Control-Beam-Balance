85023d80b6b207ec73662db67d6dfcc230325619
Kevin Zhang <kevin.zhang.13499@gmail.com>
2015-05-21 09:00:06 -0700

    sitar's code

--- BeamBalance/BeamBalance.ino
+++ BeamBalance/BeamBalance.ino
@@ -1,9 +1,8 @@
-
 #include <Servo.h> 
 
 #define sensorIR 15
 Servo myservo;  // Creates a servo object
-int pos = 0;    // Variable to store the servos angle 
+double pos = 95;    // Variable to store the servos angle 
 volatile float inches;
 //global variables
 
@@ -16,13 +15,17 @@ int pulse = 1500; //delay in microseconds
 
 double distanceFromLeft=0;  //in inches
 double distanceFromRight=0; //in inches
+
+double oldRight = 0;
+double oldLeft = 0;
+
 double TOTAL_LENGTH = 12;
 
 double currentIntegral = 0;
-double kP = 0.2;
-double kI = 0.3;
+double kP = 0.07;
+double kI = 0;
 double kD = 0; 
-double target=6;
+double target=8;
 double lasterror=0;
 double cumError=0;
 //i2c slave address AD0 pin 9 at b1101000 and b1101001
@@ -42,20 +45,38 @@ void loop() {
   double slope=error-lasterror;
   cumError=cumError+error;
   double correction = pid(error, cumError, slope, kP, kI, kD);
-  myservo.write(pos);
+  Serial.print("correction: ");
+  Serial.println(correction);
+  Serial.print("pos: ");
+  Serial.println(pos);
+  pos = getPos(pos, correction);
+  myservo.write(constrain((int)(pos+0.5),73,110));
+}
+
+double getPos(double oldPos, double correction){
+ return oldPos + correction;
+  
 }
 
 double getError(){
-  distanceFromLeft=getLeftDistance();
   distanceFromRight=getRightDistance();
+  distanceFromLeft = getLeftDistance();
   distanceFromLeft=(distanceFromLeft+(TOTAL_LENGTH-distanceFromRight))/2;
-  if(distanceFromLeft>TOTAL_LENGTH) distanceFromLeft=0;
+  Serial.print("distanceFromLeft: ");
+  Serial.println(distanceFromLeft);
+  Serial.print("distanceFromRight: ");
+  Serial.println(distanceFromRight);
+  if(distanceFromLeft > TOTAL_LENGTH) distanceFromLeft=0;
   return target-distanceFromLeft;
 }
 
 double getLeftDistance(){
   double left_ping_microseconds = ping(left_trigger_pin, left_echo_pin);
   double left_distance = ping_to_cm(left_ping_microseconds);
+  if(abs(oldLeft - left_distance) > 10){
+   return oldLeft; 
+  }
+  oldLeft = left_distance;
   return left_distance;
 }
 
@@ -66,7 +87,13 @@ double ping_to_cm(int ping_microseconds){
 double getRightDistance(){
   double right_ping_microseconds = ping(right_trigger_pin, right_echo_pin);
   double right_distance = ping_to_cm(right_ping_microseconds);
+  if(abs(oldRight - right_distance) > 10){
+   return oldRight; 
+  }
+  oldRight = right_distance;
+  return right_distance - 11;
 }
+
 double pid(double error, double integral, double derivative, double Kp, double Ki, double Kd) {
   return (error * Kp + integral * Ki + derivative * Kd);
 }
@@ -94,4 +121,3 @@ int ping(int trigger, int echo){
   // so make sure it gets enough sleep right here.  50 milliseconds
   delay(50);
   return ping_time;}
-
